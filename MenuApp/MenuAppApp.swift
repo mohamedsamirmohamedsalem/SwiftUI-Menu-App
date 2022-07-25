80cf61d568a97699d1569afc0fcb73cf4bcf73de
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2022-07-25 14:52:16 +0200

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Mohamed Samir on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
